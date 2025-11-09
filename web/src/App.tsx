39eeb020fc1984e2e9faed1f9cd1d8b3aa1618b1
Brijesh <softbrijeshio@gmail.com>
2025-11-09 10:48:26 +0530

    feat(web-page): add home page skeleton

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
+import { Home } from "@/pages";
 import "@/App.css";
 
 function App() {
   return (
     <>
-      <h1>Crew Space</h1>
+      <Home />
     </>
   );
 }
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Crew Space</h1>
+    </div>
+  );
+}
