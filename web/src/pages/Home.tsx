a7bb5fd50d5acd566e34c48189b76c7b4a7c5fa6
Brijesh <softbrijeshio@gmail.com>
2025-11-09 10:53:36 +0530

    chore: setup tailwindcss + daisyui

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export function Home() {
   return (
     <div>
-      <h1>Welcome to the Crew Space</h1>
+      <h1 className="text-3xl font-bold underline text-center mt-30">
+        Welcome to the Crew Space
+      </h1>
     </div>
   );
 }
