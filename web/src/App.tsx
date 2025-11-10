56d4aa52debb447455ed751f76438106ef3926bf
Brijesh <softbrijeshio@gmail.com>
2025-11-10 09:20:16 +0530

    chore: setup react-router-dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "@/pages";
 import "@/App.css";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
