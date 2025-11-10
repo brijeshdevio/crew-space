497352bc4ed78146614a378c3776152b15936110
Brijesh <softbrijeshio@gmail.com>
2025-11-10 09:31:19 +0530

    feat(web-page): add base layout and Tailwind styles

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,23 @@
 export function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center mt-30">
-        Welcome to the Crew Space
-      </h1>
-    </div>
+    <>
+      <section className="w-full h-screen flex items-center justify-center">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-4xl font-bold">
+            Stay in sync, without the <br />
+            noise.
+          </h1>
+          <p className="opacity-80">
+            Crew Space helps your team share focused daily updates. All signal,
+            no chat.
+          </p>
+          <div className="text-center mt-3">
+            <button className="btn btn-primary rounded-full">
+              Get started for free
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
