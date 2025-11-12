15a9bf54b59fa31e0528f146f182da2aba352ce5
Brijesh <softbrijeshio@gmail.com>
2025-11-12 10:25:48 +0530

    feat(web-page): add login page layout and Tailwind styles

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,7 +1,47 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
   return (
-    <section>
-      <h1>Crew Space</h1>
+    <section className="w-[340px] flex flex-col gap-4 p-5 rounded-2xl bg-base-200 shadow border border-white/10">
+      <div>
+        <h1 className="text-2xl font-bold">Crew Space</h1>
+        <p className="opacity-80 mt-2">Log in to your account</p>
+      </div>
+      <form className="flex flex-col gap-2">
+        <div>
+          <label className="label mb-1">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="john.doe@ex.com"
+            className="input rounded-full"
+          />
+        </div>
+        <div>
+          <label className="label mb-1">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="*********"
+            className="input rounded-full"
+          />
+        </div>
+        <div className="mt-3">
+          <button className="w-full btn btn-primary rounded-full">
+            Log in
+          </button>
+        </div>
+      </form>
+      <div className="text-center">
+        <p>
+          <span className="opacity-80">Don't have an account?</span>{" "}
+          <Link to="/register" className="link text-primary">
+            Register
+          </Link>
+        </p>
+      </div>
     </section>
   );
 }
