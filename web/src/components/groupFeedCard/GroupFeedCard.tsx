1436737d728e0b0673aaab4409262b03e081740f
Brijesh <softbrijeshio@gmail.com>
2025-11-14 12:45:47 +0530

    feat(web-component): add group feed card component

---
+++ web/src/components/groupFeedCard/GroupFeedCard.tsx
@@ -0,0 +1,16 @@
+export function GroupFeedCard() {
+  return (
+    <div>
+      <div>
+        <img src="https://placehold.co/100x100?text=O" alt="Ollvia Rhya" />
+      </div>
+      <div>
+        <div>
+          <h2>Olliva Rhya</h2>
+          <span>2 hours ago</span>
+        </div>
+        <p>Just wrap the final design for the new Dashboard.</p>
+      </div>
+    </div>
+  );
+}
--- web/src/pages/GroupFeed.tsx
+++ web/src/pages/GroupFeed.tsx
@@ -1,7 +1,10 @@
+import { GroupFeedCard } from "@/components/groupFeedCard";
+
 export function GroupFeed() {
   return (
     <section>
       <h1 className="text-2xl">Group Feed</h1>
+      <GroupFeedCard />
     </section>
   );
 }
