b76fea01621295ee4a57238342eb15563fc9d9d4
Brijesh <softbrijeshio@gmail.com>
2025-11-14 11:41:21 +0530

    feat(web-data): add dummy group data

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,9 +1,12 @@
 import { GroupCard } from "@/components/groupCard";
+import { group } from "../../data/group";
 
 export function Dashboard() {
   return (
     <section className="grid grid-cols-4 gap-3">
-      <GroupCard />
+      {group?.map((item) => (
+        <GroupCard key={item._id} />
+      ))}
     </section>
   );
 }
