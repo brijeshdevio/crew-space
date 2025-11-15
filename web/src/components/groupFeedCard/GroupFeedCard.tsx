5c4617c5b06de27eda3c5dfb20414dc3ef9abaa6
Brijesh <softbrijeshio@gmail.com>
2025-11-15 10:58:25 +0530

    feat(web-component: add tailwindcss + styles in group feed card

--- web/src/components/groupFeedCard/GroupFeedCard.tsx
+++ web/src/components/groupFeedCard/GroupFeedCard.tsx
@@ -1,15 +1,21 @@
 export function GroupFeedCard() {
   return (
-    <div>
-      <div>
-        <img src="https://placehold.co/100x100?text=O" alt="Ollvia Rhya" />
-      </div>
-      <div>
-        <div>
-          <h2>Olliva Rhya</h2>
-          <span>2 hours ago</span>
+    <div className="w-full flex gap-3 border-t border-white/10 py-3">
+      <div className="h-full">
+        <div className="avatar avatar-placeholder">
+          <div className="bg-neutral text-neutral-content w-12 rounded-full">
+            <span>OR</span>
+          </div>
         </div>
-        <p>Just wrap the final design for the new Dashboard.</p>
+      </div>
+      <div className="w-full">
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-lg font-bold">Olliva Rhya</h2>
+          <span className="badge badge-ghost">2 hours ago</span>
+        </div>
+        <p className="opacity-80">
+          Just wrap the final design for the new Dashboard.
+        </p>
       </div>
     </div>
   );
