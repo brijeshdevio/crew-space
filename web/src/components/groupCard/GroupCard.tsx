90378a54b11295fa249a3fc49d997aad081a4caf
Brijesh <softbrijeshio@gmail.com>
2025-11-16 18:35:15 +0530

    feat(web-component): style group card

--- web/src/components/groupCard/GroupCard.tsx
+++ web/src/components/groupCard/GroupCard.tsx
@@ -3,10 +3,10 @@ export function GroupCard({ name, updatedAt }: GroupType) {
   return (
     <div className="card bg-base-200 rounded-3xl">
       <div className="card-body">
-        <div className="w-full h-40 rounded-3xl bg-gradient-to-r from-orange-500 via-pink-500 to-purple-600 "></div>
+        <div className="w-full h-40 rounded-3xl bg-gradient-to-br from-indigo-500 to-cyan-500"></div>
         <div className="mt-3">
           <h3 className="text-md font-bold">{name}</h3>
-          <p className="opacity-60 mt-2 text-sm">Updated {updatedAt} ago</p>
+          <p className="opacity-60 mt-1 text-sm">Updated {updatedAt} ago</p>
         </div>
       </div>
     </div>
