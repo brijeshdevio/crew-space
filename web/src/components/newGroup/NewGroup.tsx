efdf7f2254e4fd7c95306cd383e84d53dc049fce
Brijesh <softbrijeshio@gmail.com>
2025-11-16 18:48:29 +0530

    feat(web-component): add new group modal

---
+++ web/src/components/newGroup/NewGroup.tsx
@@ -0,0 +1,40 @@
+import { X } from "lucide-react";
+
+export function NewGroup() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50 bg-base-300/80">
+      <div className="w-[350px] flex flex-col gap-4 px-5 py-5 rounded-2xl bg-base-100 shadow-md">
+        <div className="flex items-center justify-between">
+          <h2 className="text-md font-bold">Create a New Group</h2>
+          <button className="btn btn-sm btn-ghost btn-circle">
+            <X size={20} />
+          </button>
+        </div>
+        <form className="flex flex-col gap-2">
+          <div>
+            <label className="label mb-1 text-sm">Group Name</label>
+            <input
+              type="email"
+              placeholder="john.doe@ex.com"
+              className="input rounded-2xl"
+            />
+          </div>
+          <div>
+            <label className="label mb-1 text-sm">Description (Optional)</label>
+            <textarea
+              name=""
+              className="textarea resize-none rounded-2xl"
+              id=""
+              placeholder="A brief description of this group's purposes"
+            ></textarea>
+          </div>
+          <div className="mt-3">
+            <button className="w-full btn btn-primary rounded-full">
+              Create Group
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- web/src/layout/BaseLayout.tsx
+++ web/src/layout/BaseLayout.tsx
@@ -1,11 +1,13 @@
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
+import { NewGroup } from "@/components/newGroup";
 import { Outlet } from "react-router-dom";
 
 export function BaseLayout() {
   return (
     <>
       <Navbar />
+      <NewGroup />
       <main className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] max-w-[1000px] flex flex-col gap-5 mx-auto px-3 py-5">
         <Outlet />
       </main>
