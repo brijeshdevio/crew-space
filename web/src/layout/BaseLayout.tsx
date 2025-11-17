ce3eddc27bb79981e14811798a0f2ca47556aec5
Brijesh <softbrijeshio@gmail.com>
2025-11-17 10:24:44 +0530

    feat(web-component): add new post update component

--- web/src/layout/BaseLayout.tsx
+++ web/src/layout/BaseLayout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { NewGroup } from "@/components/newGroup";
+import { NewPost } from "@/components/newPost";
 import { useStore } from "@/hooks";
 import { Outlet } from "react-router-dom";
 
@@ -10,6 +11,7 @@ export function BaseLayout() {
     <>
       <Navbar />
       {isGroupModal && <NewGroup />}
+      <NewPost />
       <main className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] max-w-[1000px] flex flex-col gap-5 mx-auto px-3 py-5">
         <Outlet />
       </main>
